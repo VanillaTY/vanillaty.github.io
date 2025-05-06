ed663bd97021568dca3587ca44e3ade96b67164a
VanillaTY <503220610@qq.com>
2025-05-06 16:10:06 +0800

    feat: correct github deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = true;
-let assetPrefix = "";
-let basePath = "";
-if (isGithubActions) {
-  const repo = `yitang.github.io`.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
-  basePath,
-  assetPrefix,
-  /* config options here */
   output: "export",
 
   images: {
